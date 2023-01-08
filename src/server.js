285fb83605849c6ebd53df2d9a96741787e7b42e
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-01-08 21:50:01 +0900

    server setup

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-
+app.get("/", (req, res) => res.render("home")); // 우리 홈페이지로 이동 시 사용될 템플릿을 렌더해준다.
+app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
