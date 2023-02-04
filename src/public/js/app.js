2479706c75e1358c9020371d9294b9e7f47403a3
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-02-04 16:34:52 +0900

    Add socketIO

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,50 +1,15 @@
-// FrontEnd(Browser)
+const socket = io();
 
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick")
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-function makeMessage(type, payload) {
-    const msg = {type, payload}
-    return JSON.stringify(msg);
-}
-// connection 됐을 때
-socket.addEventListener("open", () => {
-    console.log("Connected to Server✅");
-});
-// 메시지 받았을 때 
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-// 서버가 오프라인 됐을 때
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server❌");
-});
+const handleRoomSubmit = (event) => {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+};
 
-// // 서버에게 메시지 보내기 send
-// setTimeout(() => {
-//     socket.send("Hello from the browser!");
-// }, 10000);
-
-function handleSubmit(event) {
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    // FrontEnd의 Form에서 BackEnd로 보내기
-    socket.send(makeMessage("new_message", input.value));
-    const li = document.createElement("li");
-    li.innerText = `You: ${input.value}`;
-    messageList.append(li);
-    input.value = "";
-}
-
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-}
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 
 import express from "express";
 import http from "http";
-import WebSocket, { WebSocketServer } from "ws";
+import SocketIO from "socket.io";
 const app = express();
 
 app.set("view engine", "pug");
@@ -12,38 +12,47 @@ app.get("/", (req, res) => res.render("home")); // 우리 홈페이지로 이동
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// const handleListen = () => console.log(`Listening on ws://localhost:3000`);
 
 // http 서버 위에 ws 서버 구축
-const server = http.createServer(app); // http server
-const wss = new WebSocketServer({ server }); // websocket server
+const httpServer = http.createServer(app); // http server
+const wsServer = SocketIO(httpServer);
 
-const onSocketClose = () => {
-    console.log("Disconnected from the Browser❌");
-}
-
-const sockets = [];
-// connection 이벤트를 Listen하는 곳
-// chrome, safari 끼리의 통신은 안된다. => 서버가 각각의 브라우저에 연결되어 있다.
-wss.on("connection", (socket) => {
-    // 연결된 socket(브라우저) 저장
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser✅");
-    // 브라우저가 꺼졌을 때 Listener
-    socket.on("close", onSocketClose);
-    // 브라우저가 서버에 메시지 보냈을 때 Listener
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type) {
-            case "new_message":
-                sockets.forEach((eSocket) => eSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket['nickname'] = message.payload;                
-        }
-    });
-    // // 브라우저에게 메시지 보내기
-    // socket.send("hello!!!");
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    console.log(socket.rooms);
+    socket.join(roomName);
+    console.log(socket.rooms);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
-server.listen(3000, handleListen);
+// const sockets = [];
+// // connection 이벤트를 Listen하는 곳
+// // chrome, safari 끼리의 통신은 안된다. => 서버가 각각의 브라우저에 연결되어 있다.
+// wss.on("connection", (socket) => {
+//     // 연결된 socket(브라우저) 저장
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser✅");
+//     // 브라우저가 꺼졌을 때 Listener
+//     socket.on("close", onSocketClose);
+//     // 브라우저가 서버에 메시지 보냈을 때 Listener
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type) {
+//             case "new_message":
+//                 sockets.forEach((eSocket) => eSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket['nickname'] = message.payload;
+//         }
+//     });
+//     // // 브라우저에게 메시지 보내기
+//     // socket.send("hello!!!");
+// });
+
+httpServer.listen(3000, handleListen);
