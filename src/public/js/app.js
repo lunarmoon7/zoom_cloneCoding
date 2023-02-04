f7c38530dfce07f0af04b0f6f7e1680651fea080
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-02-04 17:39:43 +0900

    NIcknames

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,14 +2,63 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-const handleRoomSubmit = (event) => {
+
+room.hidden = true;
+let roomName;
+
+const addMessage = (message) => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+};
+
+const handleMessageSubmit = (event) => {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, () => {
-    console.log("server is done!");
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
   });
   input.value = "";
 };
 
+const handleNicknameSubmit = (event) => {
+    event.preventDefault();
+    const input = choose.querySelector("#name input");
+    const value = input.value;
+    socket.emit("nickname", input.value);
+}
+
+const showRoom = () => {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  const nameForm = room.querySelector("#name");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+  nameForm.addEventListener("submit", handleNicknameSubmit);
+};
+
+const handleRoomSubmit = (event) => {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+};
+
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+  addMessage(`${user} joined!`);
+});
+
+socket.on("bye", (left) => {
+  addMessage(`${left} left🥲`);
+});
+
+socket.on("new_message", addMessage);
